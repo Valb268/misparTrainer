574b0ea35c81796299e0da4fe6f652522503a17a
Valb268 <111704264+Valb268@users.noreply.github.com>
2023-04-30 17:20:18 +0300

    Add files via upload

--- digits.js
+++ digits.js
@@ -3,6 +3,12 @@ let number = -1;
 let userAnswer = 0;
 let successInARow = 0;
 let isSubmitted = false;
+const container = document.getElementById("container");
+resizeMargin();
+window.addEventListener('resize', resizeMargin);
+function resizeMargin() {
+    container.style.marginTop = `${Math.round((visualViewport.height - container.clientHeight) / 2)}px`;
+}
 
 document.querySelector("div.btn-group").addEventListener("click", function (event) {
     if (event.target.type === "radio") {
@@ -10,6 +16,7 @@ document.querySelector("div.btn-group").addEventListener("click", function (even
     }
 });
 
+let start_button = document.getElementById('start_button');
 start_button.addEventListener('click', e => {
     // in case no digits chosen, number of digits is set randomly
     if (chosenDigits === 0) {
@@ -142,17 +149,21 @@ function submitAnswer() {
 
 function checkResult() {
     let textResult = '';
+    const par = document.createElement('p');
+    par.style.marginTop = '0';
 
     if (userAnswer === number) {
         if (!isSubmitted) {
             successInARow++;
         }
-        textResult = document.createTextNode('Верно!');
+        textResult = document.createTextNode('Correct!');
+        par.style.color = 'green';
+
     } else {
-        textResult = document.createTextNode(`Правильный ответ: ${number}`);
+        textResult = document.createTextNode(`The right answer is: ${number}`);
+        par.style.color = 'red';
         successInARow = 0;
     }
-    const par = document.createElement('p');
     par.appendChild(textResult);
 
     if (result.children.length > 0) {
@@ -164,15 +175,15 @@ function checkResult() {
 }
 
 function showSuccess() {
-    const h3 = document.createElement('h3');
-    h3.style.marginBottom = '0px';
-    h3.style.marginLeft = '2px';
-    h3.style.marginTop = '0';
+    const div = document.createElement('div');
+    // h3.style.marginBottom = '0px';
+    // div.style.marginLeft = '10px';
+    // h3.style.marginTop = '0';
     let textSuccess = document.createTextNode(successInARow);
-    h3.appendChild(textSuccess);
-    if (success.children.length > 2) {
-        success.replaceChild(h3, success.lastElementChild);
+    div.appendChild(textSuccess);
+    if (inARow.children.length) {
+        inARow.replaceChild(div, inARow.lastElementChild);
     } else {
-        success.appendChild(h3);
+        inARow.appendChild(div);
     }
 }
